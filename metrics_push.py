5afdc790c88f6bc32473072eb14a69800b705b22
herley <herley@gmail.com>
2022-12-10 10:41:57 +0700

    alarm update.

--- metrics_push.py
+++ metrics_push.py
@@ -4,16 +4,16 @@ import time
 
 cw = CloudWatchMetricsBuffer('demo_rekapitulasi_barang')
 
-cw.put_value('jumlah',5)
-cw.put_value('terjual',1)
-cw.send()
-time.sleep(300)
-
-cw.put_value('jumlah',8)
-cw.put_value('terjual',5)
-cw.send()
-time.sleep(300)
-
-# cw.put_value('jumlah',10)
-# cw.put_value('terjual',17)
+# cw.put_value('jumlah',5)
+# cw.put_value('terjual',1)
 # cw.send()
+# time.sleep(300)
+
+# cw.put_value('jumlah',8)
+# cw.put_value('terjual',5)
+# cw.send()
+# time.sleep(300)
+
+cw.put_value('jumlah',10)
+cw.put_value('terjual',17)
+cw.send()
